ea6556270b62de328b1c2da92b422dc918ec46ae
Jesse Alama <jesse.alama@gmail.com>
2011-03-18 02:03:06 +0000

    Force XHTML 1.1 DOCTYPE.
    http://www.w3.org/QA/2002/04/valid-dtd-list.html

--- utils.lisp
+++ utils.lisp
@@ -7,7 +7,7 @@
      (setf (header-out :content-type) "application/xhtml+xml; charset=UTF-8")
      (with-html-output-to-string (s nil :indent nil)
        "<?xml version='1.1' encoding='UTF-8'?>"
-       ,*prologue*
+       "<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.1//EN\" \"http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd\">"
        (htm ,@body))))
 
 (defmacro with-html (&body body)
