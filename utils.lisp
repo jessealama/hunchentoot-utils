cde1512f85a81dda94efe1f4ee4ba715f5c7bb06
Jesse Alama <jesse.alama@gmail.com>
2011-04-03 20:32:51 +0200

    Use XML 1.0 rather than 1.1.
    I'm not using the special features of XML 1.1.  Moreover, some
    browsers (notably firefox, even the most recent version 4.0) lack an
    XML 1.1 parser, and thus cannot display such XML resources.

--- utils.lisp
+++ utils.lisp
@@ -6,7 +6,7 @@
   `(progn
      (setf (header-out :content-type) "application/xhtml+xml; charset=UTF-8")
      (with-html-output-to-string (s nil :indent nil)
-       "<?xml version='1.1' encoding='UTF-8'?>"
+       "<?xml version='1.0' encoding='UTF-8'?>"
        "<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.1//EN\" \"http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd\">"
        (htm ,@body))))
 
