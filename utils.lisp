f77d8f89573043f617a5b05ded672f81724746d8
Jesse Alama <jesse.alama@gmail.com>
2011-04-14 14:30:38 +0100

    Revert.

--- utils.lisp
+++ utils.lisp
@@ -68,7 +68,7 @@
 			,doctype
 			(htm ,@body))))))))))
 
-(defmacro with-xhtml ((&rest rest
+(defmacro with-html ((&rest rest
 		       &key (content-type "text/html; charset=UTF-8")
 		            (xml-declaration "<?xml version='1.0' encoding='UTF-8'?>")
 			    (doctype "<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.1//EN\" \"http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd\">")
