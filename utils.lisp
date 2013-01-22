b3a6ed3960133bb2f62f78106a7b26be158f5b70
Jesse Alama <jesse.alama@gmail.com>
2013-01-22 16:40:19 +0100

    Wrong DTD.

--- utils.lisp
+++ utils.lisp
@@ -34,7 +34,7 @@
 (defun doctype (maybe-xhtml-version-string)
   (eswitch (maybe-xhtml-version-string :test #'string=)
     ("1.1" "<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.1//EN\" \"http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd\">")
-    ("1.0" "<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.1//EN\" \"http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd\">")))
+    ("1.0" "<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Strict//EN\" \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd\">")))
 
 (defmacro with-xml-declaration
     ((&rest rest
